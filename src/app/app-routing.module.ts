9b644a8638726407052bb27a9653f52ff81481f0
deletking <deletkingdelet@gmail.com>
2021-12-16 13:40:40 -0300

    add register modules and register component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'register',
+    loadChildren: () =>
+      import('../app/modules/register/register.module').then((m) => m.RegisterModule),
+  },
+  { path: '**', redirectTo: 'register', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
